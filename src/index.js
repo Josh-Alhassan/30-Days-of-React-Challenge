f50182eeefbbe0882c21475b9ac0a0bc6e574ef9
Saint-Josh <saintjosh26@gmail.com>
2022-08-18 13:13:26 +0100

    React Boiler plate - Rendering more JSX                            element

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-import App from './App';
+// import App from './App';
+
+const header = (
+  <header>
+    <h1>Welcome to 30 Days of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Alhassan Joshua</p>
+    <small>Aug 18, 2022</small>
+  </header>
+)
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(header, rootElement)
+
+// const root = createRoot(rootElement);
+
+
+
+
+
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
