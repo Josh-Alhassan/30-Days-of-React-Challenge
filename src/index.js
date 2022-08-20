bac6ceac51ad8851c23ac9308eb69f7765974667
Saint-Josh <saintjosh26@gmail.com>
2022-08-21 00:51:58 +0100

    Rendering component

--- src/index.js
+++ src/index.js
@@ -18,7 +18,21 @@ const date = 'Aug 18, 2022';
 // Functional Component
 
 // JSX element, header
-const header = (
+// const header = (
+//   <header>
+//     <div className="header-wrapper">
+//       <h1>Welcome to 30 Days of React</h1>
+//       <h2>Getting Started with React</h2>
+//       <h3>JavaScript Library</h3>
+//       <p>Alhassan Joshua</p>
+//       <small>Aug 20, 2022</small>
+//     </div>
+//   </header>
+// );
+
+// React component
+// Rendering components
+const Header = () => (
   <header>
     <div className="header-wrapper">
       <h1>Welcome to 30 Days of React</h1>
@@ -30,15 +44,56 @@ const header = (
   </header>
 );
 
-// React component
-const Header = () => {
-  return header;
-};
+// User Card Component - Implicit return
+const UserCard = () => (
+  <div className='user-card'>
+    <img src="https://drive.google.com/file/d/1ou4MaaVbTfNUWEiXvB_7ji_2EIMvySJ-/view?usp=sharing" alt="Photo of Joshua" />
+    <h2>Joshua Alhassan</h2>
+  </div>
+)
+
+// TechList component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript'];
+  const techsFormated = techs.map((tech) => <li key={tech}> {tech} </li>)
+  return techsFormated;
+}
+
+// Main Component
+const Main = () => (
+  <main>
+    <div className="main-wrapper">
+      <p>Prerequisite to get started react.js</p>
+      <ul>
+        <TechList />
+      </ul>
+      <UserCard />
+    </div>
+  </main>
+)
+
+// Footer Component
+const Footer = () => (
+  <footer>
+    <div className="footer-wrapper">
+      <p>Copyright 2022</p>
+    </div>
+  </footer>
+)
+
+// The App, or the parent or the container component
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
 
 const rootElement = document.getElementById('root');
 // we render the JSX element using the ReactDOM package
 // ReactDOM has the render method and the render method takes two argument
-ReactDOM.render(<Header />, rootElement);
+ReactDOM.render(<App />, rootElement);
 // or
 //  ReactDOM.render([header, main, footer], rootElement)
 
