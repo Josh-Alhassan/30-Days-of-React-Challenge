f44487c9a15e5ff969bd1075b36756047b5f0196
Saint-Josh <saintjosh26@gmail.com>
2022-08-24 09:46:49 +0100

    Destructuring props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
-import './style.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// This component shows month, date and year as an alert box
-
-// Function to display time in Mon date, year format eg Oct 4, 2020
 const showDate = (time) => {
   const months = [
     'January',
@@ -27,26 +22,46 @@ const showDate = (time) => {
   const date = time.getDate()
   return ` ${month} ${date}, ${year}`
 }
-
-// A button component
-
-const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
+// Header Component
+const Header = (props) => {
+  const data = props.data
+  const { welcome, title, subtitle, author, date } = data
+  const { firstName, lastName } = author
+  return (
+    <header>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>
+          {firstName} {lastName}
+        </p>
+        <small>{showDate(date)}</small>
+      </div>
+    </header>
+  )
+}
 
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
-  const handleTime = () => {
-    alert(showDate(new Date()))
+  const data = {
+    welcome: 'Welcome to Day 05 of 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+      firstName: 'Joshua',
+      lastName: 'Alhassan',
+    },
+    date: new Date(),
   }
-  const greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge, 2022')
-  }
+
   return (
     <div className='app'>
-      <Button text='show time' onClick={handleTime} />
-      <Button text='Greet People' onClick={greetPeople} />
+      <Header data={data} />
     </div>
   )
 }
 const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
 ReactDOM.render(<App />, rootElement)
